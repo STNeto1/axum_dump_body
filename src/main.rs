229b87fbaa085cf783461868c3297a81cd4d55fb
STNeto1 <gfreitasneto18@gmail.com>
2023-02-04 21:31:27 -0300

    dump back the body if valid

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,7 @@
 use std::net::SocketAddr;
 
-use axum::{
-    body::Body,
-    http::{Request, StatusCode},
-    response::IntoResponse,
-    routing::post,
-    Json, Router,
-};
-use serde::Serialize;
-use serde_json::{from_str, Value};
+use axum::{http::StatusCode, response::IntoResponse, routing::post, Json, Router};
+use serde_json::{from_str, json, Value};
 
 #[tokio::main]
 async fn main() {
@@ -22,34 +15,14 @@ async fn main() {
         .unwrap();
 }
 
-#[derive(Debug, Serialize)]
-struct _Response {
-    message: String,
-}
-
 async fn handle_post_request(body: String) -> impl IntoResponse {
     let parsed_body = from_str::<Value>(&body);
     if parsed_body.is_err() {
         return (
             StatusCode::BAD_REQUEST,
-            Json(_Response {
-                message: "bad request".to_owned(),
-            }),
+            Json(json!({"message": "bad body"})),
         );
     }
 
-    let body_type = match parsed_body.unwrap() {
-        Value::Null => "null".to_owned(),
-        Value::Bool(b) => b.to_string(),
-        Value::String(s) => s,
-        Value::Array(_) => "array".to_owned(),
-        Value::Object(_) => "object".to_owned(),
-        Value::Number(n) => n.to_string(),
-    };
-
-    let res = _Response {
-        message: format!("body was {}", body_type),
-    };
-
-    return (StatusCode::OK, Json(res));
+    return (StatusCode::OK, Json(parsed_body.unwrap()));
 }
